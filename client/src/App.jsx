2777d6374ee6125d8f3620f198363f249194ffb9
Manuel Verweyen <manuel.verweyen@gmail.com>
2022-01-10 13:10:00 +0100

    added components, header done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
-
+import Header from "./components/Header"
 
 export default function App() {
 
 
   return (
     <>
-    <div>Hello World</div>
+    <Header />
+        
     </>
   )
 }
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+export default function Navbar() {
+
+    return (
+        <footer>
+            <ul>
+                <li>Home</li>
+                <li>Map</li>
+                <li>Weather</li>
+                <li>Account</li>
+             </ul>
+
+        </footer>
+    )
+
+}
