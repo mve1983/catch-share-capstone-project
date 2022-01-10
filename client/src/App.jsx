971f871165c124ff5fc998790d92185460c0dfc4
Manuel Verweyen <manuel.verweyen@gmail.com>
2022-01-10 15:44:45 +0100

    added navbar with Router Navlinks

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header"
+import Navbar from "./components/Navbar"
 
 export default function App() {
 
@@ -7,6 +8,7 @@ export default function App() {
     <>
     <Header />
         
+    <Navbar />    
     </>
   )
 }
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,17 +2,58 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
+  return (
+    <Footer>
+      <AppNavbar>
+        <NavLink to="/">Home</NavLink>
 
-    return (
-        <footer>
-            <ul>
-                <li>Home</li>
-                <li>Map</li>
-                <li>Weather</li>
-                <li>Account</li>
-             </ul>
+        <NavLink to="map">Map</NavLink>
 
-        </footer>
-    )
+        <NavLink to="weather">Weather</NavLink>
 
+        <NavLink to="account">Account</NavLink>
+      </AppNavbar>
+    </Footer>
+  );
 }
+
+// styled components from here to end
+
+const Footer = styled.footer`
+  background-color: var(--background-two);
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+`;
+
+const AppNavbar = styled.nav`
+  display: flex;
+  flex-wrap: wrap;
+  flex-grow: 1;
+  flex-basis: 100%;
+  justify-content: space-evenly;
+  align-content: center;
+  align-items: center;
+
+  a {
+    font-size: 1.3rem;
+    flex-grow: 1;
+    padding: 1.3rem 0 1.3rem 0;
+    text-align: center;
+    color: black;
+    text-decoration: none;
+  }
+
+  a:hover {
+    background-color: var(--color-two);
+  }
+
+  .active {
+    background-color: var(--color-two);
+  }
+`;
