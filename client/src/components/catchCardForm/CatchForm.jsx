656f449ed78df49e53747ac907e74a06a6cb74e5
Manuel Verweyen <manuel.verweyen@gmail.com>
2022-01-18 05:16:32 +0100

    changed as per pull request review

--- client/src/components/catchCardForm/CatchForm.jsx
+++ client/src/components/catchCardForm/CatchForm.jsx
@@ -85,7 +85,7 @@ export default function CatchForm({
 
   return (
     <>
-      <FormDivBorderStyling> </FormDivBorderStyling>
+      <FormDivBorderStyling />
       <FormSection>
         <Fieldset>
           <legend>
