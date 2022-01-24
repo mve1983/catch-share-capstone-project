e29e7f54c624101f6ded513bd2e893f1c6233b93
Manuel Verweyen <manuel.verweyen@gmail.com>
2022-01-24 17:53:31 +0100

    changed mistake in Map.jsx

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -66,7 +66,7 @@ export default function Map() {
     handleInputChange("latlng", latlng);
   }
 
-  const handleSubmit = async (event) => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     setMapClicked(!mapClicked);
     formUploadSetter();
