4076a21cf8c5c93c14f1a9c2d128d33181375013
Manuel Verweyen <manuel.verweyen@gmail.com>
2022-01-27 16:44:58 +0100

    fixed bug in form

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -115,6 +115,7 @@ export default function Map() {
   });
 
   function addAdditionalCatchToMarker() {
+    setSingleCatchCard(initialCatchCard)
     setMapClicked(!mapClicked);
     addCoordinatesToCatchCard(clickedMarker.lat, clickedMarker.lng);
   }
